d287ee3c7836240339e00e607915b64c7fb6ca9a
Oscar Forero <oscar.forero@gatech.edu>
2017-05-21 10:03:22 +0800

    Add unique id generation for the Topic using the shortid library

--- src/topics/topic.go
+++ src/topics/topic.go
@@ -1,12 +1,28 @@
 package topics
 
+import (
+	"github.com/ventu-io/go-shortid"
+)
+
 type topic struct {
-	id        int
+	id        string
 	msg       string
 	upvotes   int
 	downvotes int
 }
 
+func init() {
+	sid, err := shortid.New(1, shortid.DefaultABC, 2342)
+	if err != nil {
+		panic(err)
+	}
+	shortid.SetDefault(sid)
+}
+
 func NewTopic(msg string) *topic {
-	return &topic{0, msg, 0, 0}
+	id, err := shortid.Generate()
+	if err != nil {
+		panic(err)
+	}
+	return &topic{id, msg, 0, 0}
 }
--- src/topics/topic_test.go
+++ src/topics/topic_test.go
@@ -1,11 +1,27 @@
 package topics
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestConstructTopic(t *testing.T) {
 	msg := "Test Topic"
 	tp := NewTopic(msg)
+	t.Logf("Got new message with id %v", tp.id)
 	if msg != tp.msg {
 		t.Errorf("Topic construction error: got %v want %v", tp.msg, msg)
+	}
+}
+
+func TestUniqueIds(t *testing.T) {
+	ids := make(map[string]int)
+	for i := 0; i < 10000; i++ {
+		msg := fmt.Sprintf("Topic %i", i)
+		tp := NewTopic(msg)
+		_, ok := ids[tp.id]
+		if ok {
+			t.Errorf("Repeated unique ID: %v", tp.id)
+		}
 	}
 }
